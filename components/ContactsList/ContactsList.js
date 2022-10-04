f0f343dc4174590dd00135ec77ef66011290fb33
Robert Ableson <robertableson@gmail.com>
2022-10-03 23:05:14 -0400

    Test data

--- components/ContactsList/ContactsList.js
+++ components/ContactsList/ContactsList.js
@@ -2,38 +2,38 @@ function ContactsList() {
   return(
     <div className="p-4">
       <h1 className="text-xl font-semibold pb-4">Contacts</h1>
-      
+
         <div class="overflow-x-auto relative">
             <table class="w-full text-sm text-left text-slate-500">
                 <thead class="text-xs text-slate-700 uppercase bg-slate-100">
                     <tr>
                         <th scope="col" class="py-3 px-6">
-                            Product name
+                            Business
                         </th>
                         <th scope="col" class="py-3 px-6">
-                            Color
+                            Contact
                         </th>
                         <th scope="col" class="py-3 px-6">
-                            Category
+                            Deal worth
                         </th>
                         <th scope="col" class="py-3 px-6">
-                            Price
+                            Percentage
                         </th>
                     </tr>
                 </thead>
                 <tbody>
                     <tr class="bg-white border-b">
                         <th scope="row" class="py-4 px-6 font-medium text-slate-700 whitespace-nowrap">
-                            Apple MacBook Pro 17"
+                            Apple
                         </th>
                         <td class="py-4 px-6">
-                            Sliver
+                            Steve
                         </td>
                         <td class="py-4 px-6">
-                            Laptop
+                            100,000$
                         </td>
                         <td class="py-4 px-6">
-                            $2999
+                            80%
                         </td>
                     </tr>
                 </tbody>
